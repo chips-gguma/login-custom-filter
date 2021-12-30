6a9b9c4301036740d94ad7ca8de2def23831dacd
chips-gguma <ljygguma@gmail.com>
2021-12-30 18:30:26 +0900

    AuthenticationProvider 작성

--- server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.sp.fc.web.config;
 
+import com.sp.fc.web.student.StudentManager;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,12 +11,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final StudentManager studentManager;
+
+    public SecurityConfig(StudentManager studentManager) {
+        this.studentManager = studentManager;
+    }
+
+    // auth_provider -> auth_manager에 등록
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(studentManager); // authenticationProvider를 studentManger로 지정
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(request->
-                        request.anyRequest().permitAll()
+                        request.antMatchers("/").permitAll()
+                                .anyRequest().authenticated()
+                )
+                // manager 동작을 위해 userNamePasswordFilter 동작 필요
+                .formLogin(
+                        login->login.loginPage("/login")
+                                .permitAll()
                 )
                 ;
     }
--- server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/student/StudentManager.java
@@ -1,5 +1,54 @@
 package com.sp.fc.web.student;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Set;
+
+@Component
 // 통행증을 발급할 provider
-public class StudentManager {
+public class StudentManager implements AuthenticationProvider, InitializingBean {
+
+    // 원칙적으로는 student에 대한 리스트를 db를 핸들링 하거나 가져와야 하지만 테스트 차원으로 HashMap으로 함
+    private HashMap<String, Student> studentDB = new HashMap<>();
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        // Authentication 토큰을 StudentAuthentication 토큰으로 발행하기 위해 형변환
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        if(studentDB.containsKey(token.getName())) {    // ex) hong이라는 id의 사용자가 온다면 student를 담은 StudentAuthentication 인증 토큰(통행증)을 발행함
+            Student student = studentDB.get(token.getName());
+            return StudentAuthenticationToken.builder()
+                    .principal(student)
+                    .details(student.getUser_name())
+                    .authenticated(true)
+                    .build();
+            // authenticationProvider를 athenticationManager에 등록해야 함
+        }
+        return null;    // 처리할 수 없는 authentication은 null로 넘김
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        // UsernamePasswordAuthenticationToken 형태로 토큰을 받으면 검증을 해주는 provider로 동작을 하겠다.
+        return authentication == UsernamePasswordAuthenticationToken.class;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+        Set.of(
+                new Student("hong", " 홍길동", Set.of(new SimpleGrantedAuthority("ROLE_USER"))),
+                new Student("kang", " 강아지", Set.of(new SimpleGrantedAuthority("ROLE_USER"))),
+                new Student("range", " 호랑이", Set.of(new SimpleGrantedAuthority("ROLE_USER")))
+        ).forEach(s->   // 빈이 초기화될 때마다
+                        studentDB.put(s.getId(), s)
+                );
+    }
 }
